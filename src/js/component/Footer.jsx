f43a5535f4339777fd0bc0105c171da5aa376bf5
JaimeGHE <jaimeghe2@gmail.com>
2025-01-15 18:55:52 +0000

    [sp-93] class1: navbar y footer

---
+++ src/js/component/Footer.jsx
@@ -0,0 +1,21 @@
+import React from "react";//1. Importar React.
+
+//5. Exporto mi componente.
+
+//6 y 7 Importar y utilizar el componente.
+
+
+//2. Crear el componente.
+export const Footer = () => {
+    //3. Code JS.
+
+    //4. Retorno un solo elemento HTML.
+    return (
+        <div container ><p>
+            Made by{" "}
+            <a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+            love!
+        </p>
+        </div>
+    )
+}
--- src/js/component/Home.jsx
+++ src/js/component/Home.jsx
@@ -1,5 +1,8 @@
 import React from "react"; // 1. Importo React
 import rigoImage from "../../img/rigo-baby.jpg";// Include images into your bundle
+//6 Importar el componente y 7. Utilizar
+import Navbar from "./Navbar.jsx"; //Componente con export default.
+import { Footer } from "./Footer.jsx"; //Componente con export const.
 
 //2. Create your first component with PascalCase.
 const Home = () => {
@@ -8,6 +11,7 @@ const Home = () => {
 	//4. Retorno un solo y solo un elemento HTML. 
 	return (
 		<div className="text-center">
+			<Navbar/>
 			<h1 className="text-center mt-5">Hello Rigo!</h1>
 			<p>
 				<img src={rigoImage} />
@@ -15,11 +19,7 @@ const Home = () => {
 			<a href="#" className="btn btn-success">
 				If you see this green button... bootstrap is working...
 			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+			<Footer/>
 		</div>
 	);
 };
